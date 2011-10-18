31103061d746290c801f8d6ec57fd1517de9b6c3
Eric Rochester <err8n@virginia.edu>
2011-10-17 23:18:44 -0400

    Have config files use secret key file.

--- lapsite/dev_config.py
+++ lapsite/dev_config.py
@@ -7,6 +7,8 @@ DATABASE_URI = 'sqlite:///%s/lap.db' % (CWD,)
 DATA_FILE = 'sqlite3-data.sql'
 DEBUG = True
 TESTING = False
-SECRET_KEY = '&\x1d7\xaa\xa7+\xc8A\xff\x99\x00\\\xc0\x92\xfc\xbe\xf9\x9eC,w\x86\xec\xc8'
+if os.path.exists(os.path.join(CWD, '.secret-key')):
+    with open(os.path.exists(os.path.join(CWD, '.secret-key'))) as f:
+        SECRET_KEY = f.readline().strip()
 ADMINS = ['erochest@gmail.com']
 
--- lapsite/testing_config.py
+++ lapsite/testing_config.py
@@ -8,6 +8,8 @@ DATA_FILE = 'sqlite3-data.sql'
 DATABASE_URI = 'sqlite:////tmp/lap-testing.db'
 DEBUG = True
 TESTING = True
-SECRET_KEY = 'V\xa8\x8e\x9a<Xr\x17\x0bu\xf3\xd4\xc2\x9a\xcf\xffX@\xfe\xbd|\xa8\xbe\xb6'
+if os.path.exists(os.path.join(CWD, '.secret-key')):
+    with open(os.path.join(CWD, '.secret-key')) as f:
+        SECRET_KEY = f.readline().strip()
 ADMINS = []
 
